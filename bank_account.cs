04a1a435d13323d23d0c612595f34b4a2720828c
Loskesh <lenguyenminhhoang18@gmail.com>
2025-05-16 15:27:38 +0700

    Add files via upload

---
+++ bank_account.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LTWindow_KTGK_LeNguyenMinhHoang
+{
+    public class bank_account
+    {
+        public BigInteger account_id {  get; set; }
+        public string owner_name { get; set; }
+        public string owner_adress { get; set; }
+        public string owner_phone { get; set; }
+        public double balance { get; set; }
+        public string account_type { get; set; }
+        public string password { get; set; }
+
+    }
+    public class transactions
+    {
+        public string trans_id { get; set; }
+        public DateTime happened_time { get; set; }
+        public string action_desc { get; set; }
+        public string note { get; set; }
+        public ICollection<bank_account> account_id { get; set; }
+    }
+}
